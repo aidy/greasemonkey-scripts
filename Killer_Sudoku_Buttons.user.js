88882fa36898a4293b4936fbccc7a82f2c51849a
Aidy <aidy@aidy.net>
2012-10-18 02:08:45 +0100

    Make the controls switchable with the help panel
    Rather than completely remove the help panel in order
    to inject clickable controls, provide a mechanism to
    switch between the two.

--- Killer_Sudoku_Buttons.user.js
+++ Killer_Sudoku_Buttons.user.js
@@ -3,12 +3,11 @@
 // @namespace   http://www.aidy.net
 // @description Provides buttons for killersudokuonline.com
 // @include     http://killersudokuonline.com/play.html?*
-// @version     1.1
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 
 var helpTable = document.getElementsByClassName('help')[0];
-helpTable.cellSpacing = '6px';
 
 function htmlButtons(lb) {
     var n = 0;
@@ -33,15 +32,23 @@ for (i = 1; i <= 9; i++) { nums.push([i,i]) };
 
 var keys = [ [], ['up', '&uarr;'], [], ['left', '&larr;'], ['down', '&darr;'], ['right', '&rarr;'], ['z', '⎌'], ['g', '?'], ['c', 'c']];
 
-helpTable.innerHTML = '<tbody>' 
-                      + htmlButtons(nums) 
-                      + '<tr/>'
-                      + htmlButtons(keys)
-                      + '</tbody>';
+helpTable.parentNode.innerHTML = '<div style="padding-left: 6px"><button id="ksb_toggle" onclick="return false;">Toggle controls</button></div>'
+                               + '<div id="helptable">'
+                               + helpTable.parentNode.innerHTML
+                               + '</div><div id="controltable">'
+                               + '<table><tbody>' 
+                               + htmlButtons(nums) 
+                               + '<tr/>'
+                               + htmlButtons(keys)
+                               + '</tbody></table></div>';
 
-for(i = 0; i < helpTable.rows.length; i++) {
-    helpTable.rows[i].align = 'center';
+var controlTable = document.getElementById('controltable').childNodes[0];
+for(i = 0; i < controlTable.rows.length; i++) {
+    controlTable.rows[i].align = 'center';
 }
+controlTable.cellSpacing = '6px';
+controlTable.parentNode.style.display = 'none';
+controlTable.parentNode.style.paddingRight = '40px';
 
 function addEL(id, kc) {
     document.getElementById('ksb_' + id).addEventListener(
@@ -75,3 +82,14 @@ for (i = 0; i < 4; i++) {
 [['c', 67], ['z', 90], ['g', 191]].forEach( function (k) {
     addEL(k[0], k[1]);
 });
+
+document.getElementById('ksb_toggle').addEventListener(
+    'click',
+    function () {
+        ['helptable','controltable'].forEach( function (t) {
+            var tbl = document.getElementById(t);
+            tbl.style.display = tbl.style.display ? '' : 'none';
+        })
+    },
+    false
+);
